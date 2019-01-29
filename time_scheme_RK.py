9bc0e443d1e1120531bc60e8781a25fd5bcbeac2
tegweng <32457042+tegweng@users.noreply.github.com>
2019-01-29 13:10:14 +0000

    Update time_scheme_RK.py
    adding in the different coefficients and functions for calculating dTdt and dhdt

--- time_scheme_RK.py
+++ time_scheme_RK.py
@@ -7,18 +7,25 @@ Created on Tue Jan 29 12:26:13 2019
 import numpy as np
 #Time scheme, RK4 
 
-def RK4(N_t, ):
+def RK4(N_t, dt, xi, eps, b):
     #initialise the vector
     q = np.zeros(2, N_t)
-    q[0,0] =
-    q[0,1] = 
-    for n in range(N_t +1):
-        q[n+1,n+1] = [dTdt()]
+    q[:,0] = [T_0, h_0]
+    
+    for n in range(N_t + 1):
+        k1 = [dTdt(q[0,n],q[1,n], xi, eps, b), dhdt(q[0,n], q[1,n], xi, b)]
+        k2 = []
+        k3 = []
+        k4 = []
         
-def dTdt(T, h, xi, eps):
+        q[:,n+1] = q[:, n] + dt * 1/6 * (k1 + 2 * (k2 + k3) + k4)
+        
+        
+def dTdt(T, h, xi, eps, b):
     
     R = gamma * b - c
     dTdt = R * T + gamma * h - eps * (h + b * T) ^ 3 + gamma * xi
     
-def dhdt(h, t, xi):
+def dhdt(T, h, xi, b):
     dhdt = - r * h - alpha * b * T - alpha * xi
+    
