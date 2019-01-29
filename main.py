239a924dbfd7e1036fff43369785504232aac3c7
tegweng <32457042+tegweng@users.noreply.github.com>
2019-01-29 17:56:13 +0000

    finally a working plot!!!

--- main.py
+++ main.py
@@ -4,14 +4,22 @@ Created on Tue Jan 29 16:17:23 2019
 
 @author: 25803263
 """
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 from parameter_functions import *
 from __init__ import *
 from time_scheme_RK import *
 
 
-"""
+T,h = RK4()
+
+print(T)
+
+print(h)
+
 fig = plt.figure()
-plt.plt(q)
-"""
+l1 = plt.plot(T * 7.5, h * 150,  'b-')
+plt.xlabel('T - redimenionalised')
+plt.ylabel('h - redimensionalised')
+plt.title("Ocean Recharge Oscillator")
+plt.show()
--- time_scheme_RK.py
+++ time_scheme_RK.py
@@ -13,35 +13,37 @@ from time_scheme_RK import *
 import numpy as np
 #Time scheme, RK4 
 
-def RK4(nop, dt, eps = float(0), ac = "no", wsf = "no"):
+def RK4(nop = 1, dt = 1/60, eps = float(0), ac = "no", wsf = "no"):
+    # in this space dt = 1 is equal to 2 months so 1/60 is one day
     
     #time to solution
     T = tau_c * nop
-    N_t = float(round(T/dt))
+    N_t = int(round(T/dt))
 
     #time vector
     t = np.linspace(0, N_t*dt, N_t+1) 
     
     #initialise the vector
-    q = np.zeros((int(2), int(N_t + 1)))
+    q = np.zeros((int(2), N_t + 1))
     q[:,0] = [T_0, h_0]
     
-    for n in range(int(N_t + 1)):
+    for n in range(N_t):
         #check size can work
-        k1 = [dTdt(q[0,n],q[1,n], t[n], ac, wsf, eps), dhdt(q[0,n], q[1,n], t[n], wsf)]
+        k1 = [dTdt(q[0,n],q[1,n], t[n], ac, wsf, eps), dhdt(q[0,n], q[1,n], t[n], ac, wsf)]
         
         k2 = [dTdt(q[0,n] + k1[0]* dt / 2,q[1,n] + k1[1] * dt /2 , t[n] + dt / 2, ac, wsf, eps), 
-              dhdt(q[0,n] + k1[0]* dt / 2, q[1,n] + k1[1] * dt /2 , t[n] + dt / 2, wsf)]
+              dhdt(q[0,n] + k1[0]* dt / 2, q[1,n] + k1[1] * dt /2 , t[n] + dt / 2, ac, wsf)]
         
         k3 = [dTdt(q[0,n] + k2[0]* dt / 2,q[1,n] + k2[1] * dt /2 , t[n] + dt / 2, ac, wsf, eps), 
-              dhdt(q[0,n] + k2[0]* dt / 2, q[1,n] + k2[1] * dt /2 , t[n] + dt / 2, wsf)]
+              dhdt(q[0,n] + k2[0]* dt / 2, q[1,n] + k2[1] * dt /2 , t[n] + dt / 2, ac, wsf)]
         
         k4 = [dTdt(q[0,n] + k3[0]* dt / 2,q[1,n] + k3[1] * dt /2 , t[n] + dt, ac, wsf, eps), 
-              dhdt(q[0,n] + k3[0]* dt / 2, q[1,n] + k3[1] * dt /2 , t[n] + dt, wsf)]
+              dhdt(q[0,n] + k3[0]* dt / 2, q[1,n] + k3[1] * dt /2 , t[n] + dt,ac, wsf)]
         
-        q[:,n+1] = q[:, n] + dt * 1/6 * (k1 + 2 * (k2 + k3) + k4)
+        for i in range(2):
+            q[i,n+1] = q[i, n] + dt * 1/6 * (k1[i] + 2 * (k2[i] + k3[i]) + k4[i])
         
-        return q
+    return q
         
         
 def dTdt(T, h, t, ac, wsf, eps):
@@ -52,7 +54,7 @@ def dTdt(T, h, t, ac, wsf, eps):
     
     return dTdt
     
-def dhdt(T, h, t, wsf):
+def dhdt(T, h, t, ac, wsf):
     #need to calculate b andxi here
 
     dhdt = - r * h - alpha * mew(t, ac) * T - alpha * xi(t, dt, wsf)
