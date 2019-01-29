3b0292d93ce9e2b17261968d2a47d36baf09fe96
tegweng <32457042+tegweng@users.noreply.github.com>
2019-01-29 12:30:21 +0000

    Spliting parameter variables and constants
    Made new file for parameter functions
    Added "turning on" of wind stress forcing and annual coupling parameter
    Removed epsilon from parameters as will be input to main climate model

--- __init__.py
+++ __init__.py
@@ -8,7 +8,7 @@ Created on Tue Jan 22 16:14:07 2019
 #Parameter library
 import numpy as np
 from math import sqrt
-from random import random
+
 #CONSTANTS
 
 #high end value of the coupling parameter
@@ -35,9 +35,6 @@ T_0 = 1.125 / 7.5
 
 #PARAMETERS
 
-#coupling coefficient
-mew = 2/3
-
 #relates stronger thermocline gradient to stronger easterly wind stress
 b = b_0 * mew
 
@@ -47,25 +44,10 @@ R = (gamma * b) - c
 #varies the degree of nonlinearity
 eps = 0
 
-#additional wind forcing
-xi = 0
-
 #period (nondimensionalised)
 tau_c = 2 * np.pi / omega_c
 
-#FUNCTIONS
-def mew(t, mew_0 = 0.75, mew_ann = 0.2, eps = 0.1, tau = (12/2)):
-    
-    mew = mew_0 * (1 + mew_ann * np.cos((2 * np.pi * t / tau) - (5 * np.pi / 6)))
-    
-    return mew
 
-def xi(t, dt, f_ann = 0.02, f_ran = 0.2, eps = 0.1, mew_0 = 0.75, mew_ann = 0.2, tau = (12/2), tau_cor = 1/(30*2)):
-    W = -1 + (random()*2)
-    
-    xi = f_ann * np.cos(2 * np.pi * t / tau) + f_ran * W * (tau_cor / dt)
-    
-    return xi
 
 
 
---
+++ parameter_functions.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 29 12:05:00 2019
+
+@author: 25803263
+"""
+import numpy as np
+from random import random
+
+def mew(t, mew_wac = 2/3, mew_0 = 0.75, mew_ann = 0.2, eps = 0.1, tau = (12/2),
+        ac = "yes"):
+    
+    #with annual cycle
+    if ac == "yes":
+        mew = mew_0 * (1 + mew_ann * np.cos((2 * np.pi * t / tau) - (5 * np.pi / 6)))
+    
+    #without annual cycle
+    else:
+        mew = mew_wac
+    return mew
+
+def xi(t, dt, wsf = "yes", f_ann = 0.02, f_ran = 0.2, eps = 0.1, mew_0 = 0.75, 
+       mew_ann = 0.2, tau = (12/2), tau_cor = 1/(30*2)):
+    
+    #with wind stress forcing 
+    if wsf == "yes":
+        W = -1 + (random()*2)
+    
+        xi = f_ann * np.cos(2 * np.pi * t / tau) + f_ran * W * (tau_cor / dt)
+    
+    #without wind stress forcing
+    else:
+        xi = 0
+    
+    return xi
